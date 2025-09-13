fdbdc96dcd31865e494eaaa71a45aa7fec0fb128
Rodel Paano <rodelpaano12@gmail.com>
2025-09-13 20:20:44 +0800

    update ContactPage

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -145,7 +145,9 @@ const ContactPage = () => {
                     <Mail className="h-5 w-5 text-primary" />
                     <div>
                       <p className="font-medium">Email</p>
-                      <p className="text-muted-foreground">rodel@example.com</p>
+                      <p className="text-muted-foreground">
+                        rodel09paano@gmail.com
+                      </p>
                     </div>
                   </div>
 
